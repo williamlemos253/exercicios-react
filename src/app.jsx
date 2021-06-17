1c0dcf9db7f91c84e24ad9f5404ae658cc22af4e
William Lemos de Vargas <william.lemosdevargas@gmail.com>
2021-06-17 19:03:06 -0300

    Segundo exercício

--- src/app.jsx
+++ src/app.jsx
@@ -3,11 +3,13 @@ import Primeiro from './components/basicos/Primeiro'
 
 import ComParametro from './components/basicos/ComParametro'
 import Fragmento from './components/basicos/Fragmento'
+import Aleatorio from './components/basicos/Aleatorio'
 
 export default props => {
     return (
         <div id="app">
             <h1>Fundamentos React</h1>
+            <Aleatorio min={1} max={60}/>
             <Primeiro></Primeiro>
             <ComParametro
                 titulo="Situação do Aluno"
