dad20551274019f8030cc0d5025d4c30fba07abb
William Lemos de Vargas <william.lemosdevargas@gmail.com>
2021-06-17 20:12:49 -0300

    exercícios terceira parte

--- src/app.jsx
+++ src/app.jsx
@@ -4,18 +4,30 @@ import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
 import Fragmento from './components/basicos/Fragmento'
 import Aleatorio from './components/basicos/Aleatorio'
+import Card from './components/layout/Card'
 
 export default props => {
     return (
         <div id="app">
             <h1>Fundamentos React</h1>
-            <Aleatorio min={1} max={60}/>
-            <Primeiro></Primeiro>
-            <ComParametro
-                titulo="Situação do Aluno"
-                aluno="Pedro Legal"
-                nota={4 + 4} />
-            <Fragmento></Fragmento>
+            <Card titulo="Desafio Aleatório">
+                <Aleatorio min={1} max={60} />
+            </Card>
+
+            <Card titulo="Fragmento">
+                <Fragmento></Fragmento>
+            </Card>
+            <Card titulo="Primeiro Exercício">
+                <Primeiro></Primeiro>
+            </Card>
+
+            <Card titulo="Exercício Paramêtros">
+                <ComParametro
+                    titulo="Situação do Aluno"
+                    aluno="Pedro Legal"
+                    nota={4 + 4} />
+            </Card>
+
         </div>
     );
 }
