ea6bcda569dcd80977a4439f19aec9308f9b37a1
William Lemos de Vargas <william.lemosdevargas@gmail.com>
2021-06-19 10:35:49 -0300

    exercicios children

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import Primeiro from './components/basicos/Primeiro'
 
+import Familia from './components/basicos/Familia'
+import FamiliaMembro from './components/basicos/FamiliaMembro'
+
 import ComParametro from './components/basicos/ComParametro'
 import Fragmento from './components/basicos/Fragmento'
 import Aleatorio from './components/basicos/Aleatorio'
@@ -12,6 +15,15 @@ export default props => {
         <div id="app" className="App">
             <h1>Fundamentos React</h1>
             <div className="Cards">
+
+                <Card titulo="Componentes com Filhos" color="green">
+                    <Familia sobrenome="Ferreira">
+                        <FamiliaMembro nome="Pedro"></FamiliaMembro>
+                        <FamiliaMembro nome="José" ></FamiliaMembro>
+                        <FamiliaMembro nome="Gustavo"></FamiliaMembro>
+                    </Familia>
+                </Card>
+
                 <Card titulo="Desafio Aleatório" color="green">
                     <Aleatorio min={1} max={60} />
                 </Card>
